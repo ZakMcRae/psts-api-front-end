95388df14883e9b93871a5b37323bb90d59c1df8
Zak <zakmcrae@gmail.com>
2021-04-30 15:57:52 -0400

    added static files and post routes

--- Blog_FastAPI/main.py
+++ Blog_FastAPI/main.py
@@ -1,13 +1,16 @@
 import fastapi
 import uvicorn
+from fastapi.staticfiles import StaticFiles
 
-from Blog_FastAPI.routers import user_routes
+from Blog_FastAPI.routers import user_routes, post_routes
 
 app = fastapi.FastAPI(docs_url=None, redoc_url=None)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 def configure_routing():
     app.include_router(user_routes.router)
+    app.include_router(post_routes.router)
 
 
 if __name__ == "__main__":
