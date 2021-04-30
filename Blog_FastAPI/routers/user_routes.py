e4f2b8aa3199b3005ec2e99713788dfd1de10471
Zak <zakmcrae@gmail.com>
2021-04-30 14:19:49 -0400

    Jinja2 Templates and rough homepage setup

--- Blog_FastAPI/routers/user_routes.py
+++ Blog_FastAPI/routers/user_routes.py
@@ -1,8 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+from httpx import AsyncClient
 
 router = APIRouter()
 
+templates = Jinja2Templates(directory="templates")
+
 
 @router.get("/")
-def home():
-    return "Homepage"
+async def home(request: Request):
+    async with AsyncClient(base_url="http://127.0.0.1:8000") as ac:
+        resp = await ac.get("/posts/recent?skip=0&limit=10")
+
+    posts = resp.json()
+
+    for i, post in enumerate(posts):
+        async with AsyncClient(base_url="http://127.0.0.1:8000") as ac:
+            resp = await ac.get(
+                f"/post/{post.get('id')}/replies?skip=0&limit=3&sort-newest-first=false"
+            )
+        post["replies"] = resp.json()
+
+    return templates.TemplateResponse("home.html", {"request": request, "posts": posts})
