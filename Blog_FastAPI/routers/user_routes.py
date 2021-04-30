62b67a8d55d1ac0ac575e03bf885aa68a4f50dc3
Zak <zakmcrae@gmail.com>
2021-04-30 16:02:13 -0400

    added route for
    displays user's posts and a few replies for post

--- Blog_FastAPI/routers/user_routes.py
+++ Blog_FastAPI/routers/user_routes.py
@@ -21,4 +21,32 @@ async def home(request: Request):
             )
         post["replies"] = resp.json()
 
-    return templates.TemplateResponse("home.html", {"request": request, "posts": posts})
+    return templates.TemplateResponse(
+        "home.html", {"request": request, "posts": posts, "title": "Home"}
+    )
+
+
+@router.get("/user/{user_id}")
+async def users_posts(request: Request, user_id: int):
+    async with AsyncClient(base_url="http://127.0.0.1:8000") as ac:
+        resp = await ac.get(
+            f"/user/{user_id}/posts?skip=0&limit=10&sort-newest-first=true"
+        )
+
+    posts = resp.json()
+
+    for i, post in enumerate(posts):
+        async with AsyncClient(base_url="http://127.0.0.1:8000") as ac:
+            resp = await ac.get(
+                f"/post/{post.get('id')}/replies?skip=0&limit=3&sort-newest-first=false"
+            )
+        post["replies"] = resp.json()
+
+    return templates.TemplateResponse(
+        "user_posts.html",
+        {
+            "request": request,
+            "posts": posts,
+            "title": f"{posts[0].get('username')}'s Posts",
+        },
+    )
