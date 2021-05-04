2d722eb219a74ab4f0f46d8aca620a99aa6c4ec8
Zak <zakmcrae@gmail.com>
2021-05-04 16:57:51 -0400

    separating routes to be deleted later
    these are not meant for the final site
    routes for experimenting and manual testing

--- Blog_FastAPI/main.py
+++ Blog_FastAPI/main.py
@@ -2,7 +2,7 @@ import fastapi
 import uvicorn
 from fastapi.staticfiles import StaticFiles
 
-from Blog_FastAPI.routers import user_routes, post_routes
+from Blog_FastAPI.routers import user_routes, post_routes, dev_routes
 
 app = fastapi.FastAPI(docs_url=None, redoc_url=None)
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -11,6 +11,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 def configure_routing():
     app.include_router(user_routes.router)
     app.include_router(post_routes.router)
+    app.include_router(dev_routes.router)
 
 
 if __name__ == "__main__":
