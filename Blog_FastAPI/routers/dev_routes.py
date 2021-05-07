e39a58c380adbab3561b071fb84033e847531539
Zak <zakmcrae@gmail.com>
2021-05-07 11:09:02 -0400

    verify_logged_in dependencies
    added a verify_logged_in dep. to use for auth required routes

--- Blog_FastAPI/routers/dev_routes.py
+++ Blog_FastAPI/routers/dev_routes.py
@@ -1,13 +1,14 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
+from Blog_FastAPI.util import verify_logged_in
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
 
 @router.get("/test")
-async def get_test(request: Request):
+async def get_test(request: Request, token: str = Depends(verify_logged_in)):
     """dev function - delete later"""
     return templates.TemplateResponse("test.html", {"request": request})
 
