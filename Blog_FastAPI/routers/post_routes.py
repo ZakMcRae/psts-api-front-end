ed5759f981e7c0c93fb74105b820344b2a9f66ff
Zak <zakmcrae@gmail.com>
2021-05-07 11:06:04 -0400

    new routes to create new post

--- Blog_FastAPI/routers/post_routes.py
+++ Blog_FastAPI/routers/post_routes.py
@@ -1,10 +1,49 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends, HTTPException
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
 from httpx import AsyncClient
+from starlette import status
+
+from Blog_FastAPI.util import verify_logged_in
 
 router = APIRouter()
 
 templates = Jinja2Templates(directory="templates")
+
+
+@router.get("/post/new")
+async def get_new_post(request: Request, token: str = Depends(verify_logged_in)):
+    """Create New Post - empty form unless round tripped"""
+    return templates.TemplateResponse("new_post.html", {"request": request})
+
+
+@router.post("/post/new")
+async def post_new_post(request: Request, token: str = Depends(verify_logged_in)):
+    form_data = await request.form()
+    form_data = dict(form_data)
+
+    header = {"Authorization": f"Bearer {token}"}
+    body = {
+        "title": form_data.get("title"),
+        "body": form_data.get("body"),
+    }
+
+    # send user data to backend API - create user endpoint
+    async with AsyncClient(base_url="http://127.0.0.1:8000") as ac:
+        resp = await ac.post("/post", json=body, headers=header)
+
+    # check for incorrect username or password
+    if resp.status_code != 201:
+        print(resp.status_code)
+        print(resp.json())
+
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Server Error - Try again later",
+        )
+
+    response = RedirectResponse(url=f"/post/{resp.json().get('id')}", status_code=303)
+    return response
 
 
 @router.get("/post/{post_id}")
