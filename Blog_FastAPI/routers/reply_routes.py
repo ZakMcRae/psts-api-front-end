a2a2a1797c5bcbcd561bf7a64a438ef60e5ca3b5
Zak <zakmcrae@gmail.com>
2021-05-10 12:31:51 -0400

    added reply endpoints

---
+++ Blog_FastAPI/routers/reply_routes.py
@@ -0,0 +1,135 @@
+from fastapi import APIRouter, Request, Depends, HTTPException
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+from httpx import AsyncClient
+from starlette import status
+
+from Blog_FastAPI.util import verify_logged_in
+
+router = APIRouter()
+
+templates = Jinja2Templates(directory="templates")
+
+
+# noinspection PyUnusedLocal
+# token not used by dependency confirms login
+@router.get("/post/{post_id}/reply/new")
+async def get_new_reply(request: Request, token: str = Depends(verify_logged_in)):
+    """Create New Reply - empty form unless round tripped"""
+    return templates.TemplateResponse("new_reply.html", {"request": request})
+
+
+# noinspection DuplicatedCode
+# prefer keeping routes as is for clarity
+@router.post("/post/{post_id}/reply/new")
+async def reply_new_reply(
+    request: Request, post_id: int, token: str = Depends(verify_logged_in)
+):
+    """Create New Reply - pass form data to backend api and handle any errors"""
+    form_data = await request.form()
+    form_data = dict(form_data)
+
+    # check for blank body and round trip info and errors
+    if form_data.get("body") == "":
+        form_data["body_error"] = "Body is blank"
+        return templates.TemplateResponse(
+            "new_reply.html", {"request": request, "form_data": form_data}
+        )
+
+    # send user data to backend API - create user endpoint
+    header = {"Authorization": f"Bearer {token}"}
+    body = {"body": form_data.get("body")}
+
+    async with AsyncClient(base_url="http://127.0.0.1:8000") as ac:
+        resp = await ac.post(f"/post/{post_id}/reply", json=body, headers=header)
+
+    # check for incorrect username or password
+    if resp.status_code != 201:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Server Error - Try again later",
+        )
+
+    # success - redirect to post where reply created
+    response = RedirectResponse(url=f"/post/{post_id}", status_code=303)
+    return response
+
+
+@router.get("/reply/{reply_id}/delete")
+async def get_delete_reply(
+    request: Request, reply_id: int, token: str = Depends(verify_logged_in)
+):
+    """Delete Reply - pass data to backend api and handle any errors"""
+    header = {"Authorization": f"Bearer {token}"}
+
+    async with AsyncClient(base_url="http://127.0.0.1:8000") as ac:
+        resp = await ac.delete(f"/reply/{reply_id}", headers=header)
+
+    # catch error for reply belonging to another user
+    if resp.status_code == 401:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="This reply belongs to another user",
+        )
+
+    # success - redirect home and alert user
+    response = RedirectResponse(url="/", status_code=303)
+    response.set_cookie("alert", "Reply Deleted", max_age=1)
+    return response
+
+
+# noinspection PyUnusedLocal
+# token not used by dependency confirms login
+@router.get("/reply/{reply_id}/edit")
+async def get_update_reply(
+    request: Request, reply_id: int, token: str = Depends(verify_logged_in)
+):
+    """Update Reply - filled form with existing reply info for user to modify"""
+    async with AsyncClient(base_url="http://127.0.0.1:8000") as ac:
+        resp = await ac.get(f"/reply/{reply_id}")
+
+    form_data = resp.json()
+
+    return templates.TemplateResponse(
+        "update_reply.html", {"request": request, "form_data": form_data}
+    )
+
+
+# noinspection DuplicatedCode
+# prefer keeping routes as is for clarity
+@router.post("/reply/{reply_id}/edit")
+async def post_update_reply(
+    request: Request, reply_id: int, token: str = Depends(verify_logged_in)
+):
+    """Update Reply - pass form data to backend api and handle any errors"""
+    form_data = await request.form()
+    form_data = dict(form_data)
+
+    # check for blank body and round trip info and errors
+    if form_data.get("body") == "":
+        form_data["body_error"] = "Body is blank"
+        return templates.TemplateResponse(
+            "update_reply.html", {"request": request, "form_data": form_data}
+        )
+
+    # send user data to backend API - create user endpoint
+    header = {"Authorization": f"Bearer {token}"}
+    body = {"body": form_data.get("body")}
+
+    async with AsyncClient(base_url="http://127.0.0.1:8000") as ac:
+        resp = await ac.put(f"/reply/{reply_id}", json=body, headers=header)
+
+    # catch error for reply belonging to another user
+    if resp.status_code == 401:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="This reply belongs to another user",
+        )
+
+    reply_info = resp.json()
+
+    # success - redirect to post with updated reply
+    response = RedirectResponse(
+        url=f"/post/{reply_info.get('post_id')}", status_code=303
+    )
+    return response
