bfd18f6dae737c3ba52a6535095ad364e29086de
Zak <zakmcrae@gmail.com>
2021-05-10 12:29:11 -0400

    included reply routes endpoints

--- Blog_FastAPI/main.py
+++ Blog_FastAPI/main.py
@@ -2,7 +2,7 @@ import fastapi
 import uvicorn
 from fastapi.staticfiles import StaticFiles
 
-from Blog_FastAPI.routers import user_routes, post_routes, dev_routes
+from Blog_FastAPI.routers import user_routes, post_routes, dev_routes, reply_routes
 
 # todo turn off docs urls after dev work finished - uncomment below
 app = fastapi.FastAPI()  # docs_url=None, redoc_url=None)
@@ -12,6 +12,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 def configure_routing():
     app.include_router(user_routes.router)
     app.include_router(post_routes.router)
+    app.include_router(reply_routes.router)
     app.include_router(dev_routes.router)
 
 
