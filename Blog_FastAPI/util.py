8d772460907203464491fa631c44c2692554b9d0
Zak <zakmcrae@gmail.com>
2021-05-12 12:25:17 -0400

    added function that returns user info from token

--- Blog_FastAPI/util.py
+++ Blog_FastAPI/util.py
@@ -1,4 +1,5 @@
 from fastapi import Request, HTTPException
+from httpx import AsyncClient
 from starlette import status
 
 
@@ -12,3 +13,25 @@ async def verify_logged_in(request: Request) -> str:
         )
 
     return request.cookies.get("jlt")
+
+
+async def get_user_info(request: Request):
+    """Get user info if currently logged in from token/cookie - returns dict with 'id':0 if not logged in. Using to
+    compare if logged in user_id is the same as post.user_id and then display links to edit or delete own content."""
+    if request.cookies.get("jlt"):
+        header = {"Authorization": f"Bearer {request.cookies.get('jlt')}"}
+        async with AsyncClient(base_url="http://127.0.0.1:8000") as ac:
+            resp = await ac.get("/user/me", headers=header)
+
+        if resp.status_code == 401:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail=resp.json().get("detail"),
+            )
+
+        user_info = resp.json()
+
+    else:
+        user_info = {"id": 0}
+
+    return user_info
